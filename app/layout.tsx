36a5bc60881c8b96aede972297ea8b5629abe3cb
Daniel Kim <khs12100@gmail.com>
2022-12-27 06:06:04 -0500

    changed page to ssr

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +12,29 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="flex w-full flex-col items-center">
+          <div className="m-5 max-w-full rounded-3xl border bg-blue-200 bg-gradient-to-br from-yellow-200 p-10 text-center text-7xl font-bold text-gray-700 ">
+            Project by Kimbros
+          </div>
+          <div className="flex w-full items-center justify-evenly">
+            <div className="space-x-5 rounded-3xl border bg-white p-6 text-center text-4xl text-red-900">
+              hello world
+            </div>
+            <button className="btn-primary btn-circle btn mt-8 h-28 w-28 text-yellow-200">
+              fking useless button
+            </button>
+            <div className="stats shadow">
+              <div className="stat">
+                <div className="stat-title">AMC projected price</div>
+                <div className="stat-value">$500,000</div>
+                <div className="stat-desc">literally on the moon</div>
+              </div>
+            </div>
+          </div>
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import { Inter } from "@next/font/google";
 import prisma from "../lib/prisma";
+
+export const dynamic = 'force-dynamic';
 
 async function fetchData() {
   const feed = await prisma.post.findMany({
@@ -16,29 +17,9 @@ async function fetchData() {
 
 export default async function Home() {
   const postList = await fetchData();
-  console.log(postList);
 
   return (
     <div className="flex w-full flex-col items-center">
-      <div className="m-5 max-w-full rounded-3xl border bg-blue-200 bg-gradient-to-br from-yellow-200 p-10 text-center text-7xl font-bold text-gray-700 ">
-        Project by Kimbros
-      </div>
-      <div className="flex w-full items-center justify-evenly">
-        <div className="space-x-5 rounded-3xl border bg-white p-6 text-center text-4xl text-red-900">
-          hello world
-        </div>
-        <button className="btn-primary btn-circle btn mt-8 h-28 w-28 text-yellow-200">
-          fking useless button
-        </button>
-        <div className="stats shadow">
-          <div className="stat">
-            <div className="stat-title">AMC projected price</div>
-            <div className="stat-value">$500,000</div>
-            <div className="stat-desc">literally on the moon</div>
-          </div>
-        </div>
-      </div>
-
       <h1 className="self-start p-5 pt-20 text-7xl text-gray-100">
         Cards for List Page
       </h1>
