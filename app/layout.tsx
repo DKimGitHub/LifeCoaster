7d728ecb090aa82975b0fab6d047c547a85c25ea
Daniel Kim <khs12100@gmail.com>
2022-12-30 03:34:54 -0500

    added navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,30 @@ export default function RootLayout({
       */}
       <head />
       <body>
+        <div className="navbar !fixed bg-purple-400 z-50">
+          <div className="flex-1">
+            <a className="btn-ghost btn text-xl normal-case">LifeCoaster</a>
+          </div>
+          <div className="flex-none">
+            <button className="btn-ghost btn-square btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-5 w-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
         <div className="flex w-full flex-col items-center">
-          <div className="m-5 max-w-full rounded-3xl border bg-blue-200 bg-gradient-to-br from-yellow-200 p-10 text-center text-7xl font-bold text-gray-700 ">
+          <div className="m-4 mt-20 max-w-full rounded-3xl border bg-blue-200 bg-gradient-to-br from-yellow-200 p-10 text-center text-7xl font-bold text-gray-700 ">
             Project by Kimbros
           </div>
           <div className="flex w-full items-center justify-evenly">
