a97fb6e7517bff5bac3ef473f77d8a2a395b7a82
Daniel Kim <khs12100@gmail.com>
2022-12-31 08:21:47 -0500

    minor fix

--- components/AuthButtonHeader.tsx
+++ components/AuthButtonHeader.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 export default function AuthButtonHeader() {
   const { data: session } = useSession();
