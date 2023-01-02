bd8a4f4bbf825db18e90fa0362b2559aaf33c409
Daniel Kim <khs12100@gmail.com>
2023-01-02 10:13:25 -0500

    update schema

--- components/AuthButtonHeader.tsx
+++ components/AuthButtonHeader.tsx
@@ -1,12 +1,32 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 export default function AuthButtonHeader() {
   const { data: session } = useSession();
   return session ? (
-    <button className="btn" onClick={() => signOut()}>
-      logout
-    </button>
+    <>
+      <div className="dropdown-bottom dropdown-end dropdown">
+        <label tabIndex={0} className="btn">
+          Me
+        </label>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu rounded-box w-52 bg-base-100 p-2 mt-1 shadow"
+        >
+          <li>
+          <Link href="/profile" className="btn btn-ghost">
+              My Profile
+            </Link>
+          </li>
+          <li>
+            <button className="btn btn-ghost" onClick={() => signOut()}>
+              logout
+            </button>
+          </li>
+        </ul>
+      </div>
+    </>
   ) : (
     <button className="btn" onClick={() => signIn()}>
       Login
