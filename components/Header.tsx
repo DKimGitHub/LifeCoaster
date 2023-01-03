00a757013d9592143c0187593f240aab836ed277
Daniel Kim <khs12100@gmail.com>
2023-01-03 07:08:27 -0500

    responsive layout and temp list page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,36 @@
 import Link from "next/link";
-import AuthButtonHeader from "./AuthButtonHeader"
+import AuthButtonHeader from "./AuthButtonHeader";
 
 export default function Header() {
-
   return (
-    <header className="navbar z-50 shadow-md py-3 pr-6 sm:px-8 md:px-16 lg:px-36">
-      <div className="flex-1">
-        <Link className="btn-ghost btn text-3xl normal-case text-purple-900 hover:text-black" href="/">LifeCoaster</Link>
-      </div>
-      <div className="flex-none">
-      <ul className="menu menu-horizontal pr-4">
-      <li><Link href="/create" className="text-xl text-purple-900 font-bold rounded-md leading-none hover:text-black">create</Link></li>
-      <li><Link href="/list" className="text-xl text-purple-900 font-bold rounded-md leading-none hover:text-black">list</Link></li>
-      </ul>
-      <AuthButtonHeader/>
+    <header className="navbar z-50 py-3 shadow-md">
+      <div className="mx-auto w-full max-w-6xl px-4">
+        <div className="flex-1">
+          <Link
+            className="btn-ghost btn text-3xl normal-case text-purple-900 hover:text-black"
+            href="/">
+            LifeCoaster
+          </Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal pr-4">
+            <li>
+              <Link
+                href="/create"
+                className="rounded-md text-xl font-bold leading-none text-purple-900 hover:text-black">
+                create
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/list"
+                className="rounded-md text-xl font-bold leading-none text-purple-900 hover:text-black">
+                list
+              </Link>
+            </li>
+          </ul>
+          <AuthButtonHeader />
+        </div>
       </div>
     </header>
   );
