a58d7b24a91fbe065690d42408a8582a0d3516ab
Kisang Kim <ryankim0117@gmail.com>
2023-01-03 19:18:11 -0500

    chartjs working

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -8,7 +8,7 @@ import { findAncestor } from "typescript";
 
 export default function Page() {
   //const [formOutput, setFormOutput] = useState([]);
-  const [data, setData] = useState<Array<FormState>>([]);
+  const [userInput, setUserInput] = useState<Array<FormState>>([{year: 1995, value: 0}]);
 
   interface FormState{
     year : number;
@@ -21,8 +21,8 @@ export default function Page() {
     const formElements = form.elements;
     const year : number = parseInt(formElements.yearInput.value);
     const value : number = parseInt(formElements.valueInput.value);
-    if (data.find(item => item.year === year) === undefined){
-      setData(prevData => [...prevData, {year: year, value: value}]);
+    if (userInput.find(item => item.year === year) === undefined){
+      setUserInput(prev => [...prev, {year: year, value: value}]);
     }
     else{
       alert("The year overlaps!");
@@ -30,10 +30,12 @@ export default function Page() {
 		
   }
 
+  function formValidation 
+
   return (
     <div className="flex flex-col items-center">
       <div className="mt-10 aspect-[21/5] w-full border border-black text-center">
-			<Graph data={data}/>
+			<Graph data={userInput}/>
       </div>
       <div className={createStyles.formContainer}>
         <form onSubmit={handleSubmit}>
@@ -44,7 +46,7 @@ export default function Page() {
           <input className={createStyles.formInput} id="valueInput" type="text" />
           <br />
           <input
-            className={createStyles.formInput}
+            className={createStyles.formSubmit}
             type="submit"
             value="Submit"
           />
