707baff4041e5be1346efe866f63e42cb0aa5786
Daniel Kim <khs12100@gmail.com>
2023-01-05 19:08:58 -0500

    change routing to \u and \p

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,11 @@ import AuthButtonHeader from "./AuthButtonHeader";
 
 export default function Header() {
   return (
-    <header className="navbar z-50 py-3 shadow-md">
+    <header className="navbar min-h-12 h-12 z-50 py-1 shadow-md">
       <div className="mx-auto w-full max-w-6xl px-4">
         <div className="flex-1">
           <Link
-            className="btn-ghost btn text-3xl normal-case text-purple-900 hover:text-black"
+            className="btn-ghost btn text-2xl normal-case"
             href="/">
             LifeCoaster
           </Link>
@@ -17,14 +17,14 @@ export default function Header() {
             <li>
               <Link
                 href="/create"
-                className="rounded-md text-xl font-bold leading-none text-purple-900 hover:text-black">
+                className="rounded-md text-xl font-bold leading-none ">
                 create
               </Link>
             </li>
             <li>
               <Link
                 href="/list"
-                className="rounded-md text-xl font-bold leading-none text-purple-900 hover:text-black">
+                className="rounded-md text-xl font-bold leading-none">
                 list
               </Link>
             </li>
