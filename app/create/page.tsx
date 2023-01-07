22d5fae9c8c553cbfc51256c51eabe90d76bc6fc
Kisang Kim <ryankim0117@gmail.com>
2023-01-07 18:51:09 -0500

    api testing

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -4,21 +4,50 @@ import React, { useState, createContext } from "react";
 import createStyles from "../../styles/create.module.css";
 import Graph from "../../components/Graph";
 import CreateForm from "../../components/CreateForm"
+import useSWR from 'swr'
 import { findAncestor } from "typescript";
+import { useFetcher } from "react-router-dom";
+import { json } from "stream/consumers";
 
 export const CreateContext = createContext("defaultValue");
 
 export default function Page() {
-
-  const [userInput, setUserInput] = useState<FormState[]>([
-    { year: 1995, value: 0 },
-  ]);
-
+  //Form Type
   interface FormState {
     year: number;
     value: number;
   }
 
+  //Node states
+  const [userInput, setUserInput] = useState<FormState[]>([
+    { year: 1995, value: 0 },
+  ]);
+
+  
+  //fetcher function for SWR
+  const fetcher = async (
+    url: string,
+    method: string
+  ) => {
+    const options = {
+      method: method,
+      body: JSON.stringify(
+        {
+          year: 1995,
+          value: -10
+        }
+      )
+    }
+    return fetch(url, options).then(r => r.json());
+  }
+
+  const { data, error } = useSWR(['/api/graph', "POST"], ([url, method]) => fetcher(url, method))
+  
+ 
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  //Function sent through ContextProvider for changing the node states
   function updateUserInput (input:FormState[]) {
     setUserInput(input);
   }
