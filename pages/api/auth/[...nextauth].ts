cd072ae92b0d4386c53eedfcf85178d13a54bdbf
Daniel Kim <khs12100@gmail.com>
2023-01-07 12:30:18 -0500

    vercel build fail fix maybe?

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
-import GoogleProvider from "next-auth/providers/Google";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../lib/prisma";
 
