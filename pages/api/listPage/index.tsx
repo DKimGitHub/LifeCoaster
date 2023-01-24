6b6834fdc63231924700c4f0611ab49810850dd9
Daniel Kim <khs12100@gmail.com>
2023-01-24 16:23:28 -0500

    minor

--- pages/api/listPage/index.tsx
+++ pages/api/listPage/index.tsx
@@ -33,6 +33,7 @@ export default async function handler(
             },
           },
         },
+        //@ts-ignore
         orderBy: {
           ...(sortBy === "RC"
             ? { createdAt: "desc" }
