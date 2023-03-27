d5e37f1ed8155465d4e382753664975bd0c04b24
Daniel Kim <khs12100@gmail.com>
2023-03-27 13:22:48 -0400

    filler homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ export default async function Home() {
       <div className="mt-4 max-w-full rounded-xl border bg-blue-200 bg-gradient-to-br from-yellow-200 p-10 text-center text-7xl font-bold text-gray-700 ">
         Project by Kimbros
       </div>
+      <div className="w-full h-64 flex flex-col items-center">
+        <p className="mt-2 text-5xl font-semibold">Build your own LifeCoaster!
+        </p>
+        <p className="mt-2 text-3xl font-medium"> Ride through the ups and downs of your life~
+        </p>
+      </div>
     </>
   );
 }
