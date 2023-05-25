7ab6916f5295a3a902ff09afc1e056e0d68096dd
Daniel Kim <khs12100@gmail.com>
2023-05-25 08:04:57 -0400

    postid vs postId bug fix

--- app/p/[postId]/page.tsx
+++ app/p/[postId]/page.tsx
@@ -3,9 +3,10 @@ import Navigation from "../../../components/Navigation";
 import PostPage from "../../../components/postPage/PostPage";
 import getPostPageData from "../../../lib/getPostPageData"
 
-export default async function Page({ params }: { params: { postid: string } }) {
-  const { postid } = params;
-  const { postData } = await getPostPageData(postid);
+//WARNING it's postId for p\ and it's postid for (..)p\ 
+export default async function Page({ params }: { params: { postId: string } }) {
+  const { postId } = params;
+  const { postData } = await getPostPageData(postId);
   return (
     <>
       <Navigation />
