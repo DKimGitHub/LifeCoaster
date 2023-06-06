9688ed75c7552d5e678d98dca3119f730606f6fd
Kisang Kim <ryankim0117@gmail.com>
2023-06-06 06:07:28 -0400

    tootlip

--- components/CustomToolTip.tsx
+++ components/CustomToolTip.tsx
@@ -1,4 +1,5 @@
 import { PointTooltipProps } from "@nivo/line";
+import { TooltipWrapper } from '@nivo/tooltip';
 import { mockData1 } from "../lib/mockData";
 import { Pangolin } from "next/font/google";
 
@@ -13,20 +14,22 @@ export default function CustomToolTip(props: PointTooltipProps) {
   // const data = mockData1[0].data.find(e=> e.y ===p.y)?.title;
 
   return (
-    <div
-      className={pangolin.className}
-      style={{
-        border: "2px solid #ff297b",
-        padding: "0.5rem",
-        borderRadius: "0.5rem",
-        color: "#704f47",
-        fontWeight: "600"
-      }}>
-      <span>Year: {p.xFormatted}</span>
-      <br />
-      <span>
-        Satisfactory: {p.yFormatted}
-      </span>
-    </div>
+    <TooltipWrapper anchor="left" position={[0, 0]}>
+      <div
+        className={pangolin.className}
+        style={{
+          border: "2px solid #ff297b",
+          padding: "0.5rem",
+          borderRadius: "0.5rem",
+          color: "#704f47",
+          fontWeight: "600"
+        }}>
+        <span>Year: {p.xFormatted}</span>
+        <br />
+        <span>
+          Satisfactory: {p.yFormatted}
+        </span>
+      </div>
+    </TooltipWrapper>
   );
 }
